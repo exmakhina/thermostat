3c1ca8ba25ed8bdb87f31902b148107aeee0d19d
Thomas <tgrimault@tamaggo.com>
2013-10-09 21:59:45 -0400

    make the serial port access thread-safe.

--- host/src/serial_if.c
+++ host/src/serial_if.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
+#include <pthread.h> /* Mutex stuff */
 
 #include "crc8.h"
 #include "serial_if.h"
@@ -13,11 +14,15 @@
 #define TRANSACTION_SIZE	4
 
 static uint8_t serial_buffer[TRANSACTION_SIZE];
+static pthread_mutex_t serial_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int serial_init(void)
 {
 	struct termios tio;
     int tty_fd = 0;
+    
+    /* creates a mutex */
+    if (!pthread_mutex_init(&serial_mutex, NULL)) return -1;
         
 	memset(&tio,0,sizeof(tio));
     tio.c_iflag=0;
@@ -27,18 +32,25 @@ int serial_init(void)
     tio.c_cc[VMIN]=1;
     tio.c_cc[VTIME]=0;		/* MIN > 0; TIME == 0: read(2) blocks until the lesser of MIN bytes or the number of bytes requested are available, and returns the lesser of these two values. */
 
+	pthread_mutex_lock(&serial_mutex);
     tty_fd=open(SERIAL_IF_PORT, O_RDWR | O_NONBLOCK);
     if (tty_fd != -1) {      
 		cfsetospeed(&tio,SERIAL_IF_BAUD_RATE);            // 9600 baud
 		cfsetispeed(&tio,SERIAL_IF_BAUD_RATE);            // 9600 baud
     }
+    pthread_mutex_unlock(&serial_mutex);
     
     return tty_fd;
 }
 
 void serial_close(int tty_fd)
 {
+	pthread_mutex_lock(&serial_mutex);
 	close(tty_fd);
+	pthread_mutex_unlock(&serial_mutex);
+	
+	/* destroys the mutex */
+    pthread_mutex_destroy(&serial_mutex);
 }
 
 /* Return -1 if fails, 0 if success                */
@@ -49,7 +61,10 @@ int receive_transaction(int tty_fd, transaction_t * transaction)
 	
 	if (transaction == NULL) return E_SYS;
 	
+	pthread_mutex_lock(&serial_mutex);
 	result = read(tty_fd, serial_buffer, TRANSACTION_SIZE);
+	pthread_mutex_unlock(&serial_mutex);
+	
 	if (result < TRANSACTION_SIZE)
 		return E_NOT_READY; 		// transaction not complete
 	
@@ -86,7 +101,10 @@ int send_response(int tty_fd, transaction_t * transaction)
 	serialized[2] = transaction->value & 0xff;
 	serialized[3] = crc8(serialized, 3);
 	
+	pthread_mutex_lock(&serial_mutex);
 	res = write(tty_fd, serialized, TRANSACTION_SIZE);
+	pthread_mutex_unlock(&serial_mutex);
+	
 	if (res < TRANSACTION_SIZE)
 		return E_SYS;
 	
